d4f787151ae71945afb721ff930c3b96adee1b35
mamarasulov <bvjcjcgjf@gmail.com>
2025-05-16 12:03:22 +0600

    colors

---
+++ lib/src/core/constants/colors.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color whiteColorLight = Color(0xFFFAFAFA);
+  static const Color blackColorLight = Color(0xFF252525);
+
+  static const Color primaryColor = Color(0xFF3F00C0);
+  static const Color primaryColorLight = Color(0xFF661EFB);
+
+  static const Color greyColor = Color(0xFF878585);
+  static const Color greyColorLight = Color(0xFFCACACA);
+  static const Color greyerColor = Color(0xFF767676);
+
+  static const Color violetColor = Color(0xFFBC6FF8);
+  static const Color blueColor = Color(0xFF4600D7);
+  static const Color blueColorLight = Color(0xFF8485F2);
+
+  static const Color nextbackColor = Color(0xFFF2EFFF);
+  static const Color redColor = Color(0xFFD81112);
+  static const Color redColorLight = Color(0xFFFFE0E0);
+
+  static const Color greenColor = Color(0xFF0F913F);
+  static const Color greenColorLight = Color(0xFFE6FFF0);
+
+  static const Color solePropColor = Color(0xFF7B37B5);
+  static const Color llsColor = Color(0xFFF21AA2);
+  static const Color salesColor = Color(0xFF156CB1);
+  static const Color litigationColor = Color(0xFFCCC9AA);
+  static const Color investmentColor = Color(0xFF20BF93);
+  static const Color revenueColor = Color(0xFFFCA12D);
+}
