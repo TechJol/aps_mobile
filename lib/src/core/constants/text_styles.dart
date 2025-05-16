35db540f809f7f0c64d3e9155d92a457c077810b
mamarasulov <bvjcjcgjf@gmail.com>
2025-05-16 12:11:55 +0600

    text styles added

---
+++ lib/src/core/constants/text_styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const f9w400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 9);
+
+  static const f12w400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 12);
+  static const f12w500 = TextStyle(fontWeight: FontWeight.w500, fontSize: 12);
+
+  static const f14w400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 14);
+  static const f14w500 = TextStyle(fontWeight: FontWeight.w500, fontSize: 14);
+  static const f14w600 = TextStyle(fontWeight: FontWeight.w600, fontSize: 14);
+
+  static const f16w400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 16);
+  static const f16w500 = TextStyle(fontWeight: FontWeight.w500, fontSize: 16);
+  static const f16w600 = TextStyle(fontWeight: FontWeight.w600, fontSize: 16);
+
+  static const f18w400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 18);
+  static const f18w500 = TextStyle(fontWeight: FontWeight.w500, fontSize: 18);
+  static const f18w600 = TextStyle(fontWeight: FontWeight.w600, fontSize: 18);
+
+  static const f20w400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 20);
+  static const f20w500 = TextStyle(fontWeight: FontWeight.w500, fontSize: 20);
+  static const f20w600 = TextStyle(fontWeight: FontWeight.w600, fontSize: 20);
+  static const f20w700 = TextStyle(fontWeight: FontWeight.w700, fontSize: 20);
+
+  static const f24w400 = TextStyle(fontWeight: FontWeight.w400, fontSize: 24);
+  static const f24w600 = TextStyle(fontWeight: FontWeight.w600, fontSize: 24);
+  static const f24w700 = TextStyle(fontWeight: FontWeight.w700, fontSize: 24);
+
+  static const f26w600 = TextStyle(fontWeight: FontWeight.w600, fontSize: 26);
+  static const f28w500 = TextStyle(fontWeight: FontWeight.w500, fontSize: 28);
+  static const f34w600 = TextStyle(fontWeight: FontWeight.w600, fontSize: 34);
+}
