5fffbbae10702765756244551d6e85920c0714e0
mamarasulov <bvjcjcgjf@gmail.com>
2025-05-16 12:41:04 +0600

    dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,6 @@
+import 'package:aps_mobile/src/feature/feature.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Aps Mobile',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Main(),
-    );
-  }
-}
-
-class Main extends StatelessWidget {
-  const Main({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Placeholder();
-  }
 }
