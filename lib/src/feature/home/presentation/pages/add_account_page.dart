e9b5877219a53cc8cc58cb465fa9d9b7e61e5831
mamarasulov <bvjcjcgjf@gmail.com>
2025-05-21 11:57:01 +0600

    add account added

--- lib/src/feature/home/presentation/pages/add_account_page.dart
+++ lib/src/feature/home/presentation/pages/add_account_page.dart
@@ -8,6 +8,7 @@ class AddAccountPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColors.backroundColor,
         centerTitle: false,
         title: Text('Счета', style: AppTextStyles.f24w600),
         leadingWidth: 100,
@@ -38,42 +39,62 @@ class AddAccountPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            20.h,
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton.icon(
-                    onPressed: () {},
-                    label: const Text(
-                      'Добавить счет',
-                      style: AppTextStyles.f16w500,
-                    ),
+      body: Column(
+        children: [
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: AppColors.backroundColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  20.h,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton.icon(
+                          onPressed: () {},
+                          label: const Text(
+                            'Добавить счет',
+                            style: AppTextStyles.f16w500,
+                          ),
 
-                    icon: const Icon(Icons.add, size: 20),
-                    iconAlignment: IconAlignment.end,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColors.primaryColorLight,
-                      foregroundColor: Colors.white,
-                      fixedSize: Size(double.infinity, 48),
-                    ),
+                          icon: const Icon(Icons.add, size: 20),
+                          iconAlignment: IconAlignment.end,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.primaryColorLight,
+                            foregroundColor: Colors.white,
+                            fixedSize: Size(double.infinity, 48),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  12.h,
+                  Row(
+                    children: [
+                      OutlinedButtonWidget(
+                        onPressed: () {},
+                        text: 'Распечатать',
+                      ),
+                      12.w,
+                      OutlinedButtonWidget(
+                        onPressed: () {},
+                        text: 'Скачать в Excel',
+                      ),
+                    ],
+                  ),
+                  20.h,
+                ],
+              ),
             ),
-            12.h,
-            Row(
-              children: [
-                OutlinedButtonWidget(onPressed: () {}, text: 'Распечатать'),
-                12.w,
-                OutlinedButtonWidget(onPressed: () {}, text: 'Скачать в Excel'),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
