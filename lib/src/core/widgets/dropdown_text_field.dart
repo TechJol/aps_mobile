faf0152d055703037a502d0649e347a3ae420739
mamarasulov <bvjcjcgjf@gmail.com>
2025-05-24 10:41:21 +0600

    accaount add page completed but not all

--- lib/src/core/widgets/dropdown_text_field.dart
+++ lib/src/core/widgets/dropdown_text_field.dart
@@ -1,59 +1,51 @@
 import 'package:aps_mobile/src/core/core.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 
-class DropdownFormField extends StatelessWidget {
-  const DropdownFormField({
-    super.key,
-    required this.currencies,
-    this.value,
-    this.onChanged,
-    required this.label,
-  });
+class DropDownFormField extends StatelessWidget {
+  const DropDownFormField({super.key, required this.items});
 
-  final List<String> currencies;
-  final String? value;
-  final void Function(String?)? onChanged;
-  final String label;
+  final List<String> items;
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField<String>(
-      icon: const SizedBox.shrink(),
-      value: value,
-      decoration: InputDecoration(
-        suffixIcon: Icon(
-          Icons.keyboard_arrow_down_outlined,
-          size: 30,
-          color: AppColors.greyerColor,
-        ),
-        labelText: label,
-        fillColor: AppColors.backroundColor,
-        filled: true,
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: const BorderSide(color: AppColors.backroundColor),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: const BorderSide(
-            width: 1,
-            color: AppColors.backroundColor,
-          ),
-        ),
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 14,
+    return DropdownSearch<String>(
+      items: (f, cs) => items,
+      suffixProps: DropdownSuffixProps(
+        dropdownButtonProps: DropdownButtonProps(
+          iconClosed: Icon(Icons.keyboard_arrow_down_outlined),
+          iconOpened: Icon(Icons.keyboard_arrow_up_outlined),
         ),
       ),
-      items:
-          currencies
-              .map(
-                (currency) =>
-                    DropdownMenuItem(value: currency, child: Text(currency)),
-              )
-              .toList(),
-      onChanged: onChanged,
+      decoratorProps: DropDownDecoratorProps(
+        decoration: InputDecoration(
+          label: Text('Название'),
+          fillColor: AppColors.backroundColor,
+          filled: true,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(25)),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: const BorderSide(color: AppColors.backroundColor),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: const BorderSide(
+              width: 1,
+              color: AppColors.backroundColor,
+            ),
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 14,
+          ),
+        ),
+      ),
+      popupProps: PopupProps.menu(
+        showSelectedItems: true,
+
+        disabledItemFn: (item) => item == 'Item 3',
+        fit: FlexFit.loose,
+      ),
     );
   }
 }
--- lib/src/feature/home/presentation/pages/add_account_page.dart
+++ lib/src/feature/home/presentation/pages/add_account_page.dart
@@ -9,46 +9,27 @@ class AddAccountPage extends StatefulWidget {
 }
 
 class _AddAccountPageState extends State<AddAccountPage> {
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController typeController = TextEditingController();
-  final TextEditingController currencyController = TextEditingController();
+  final List<String> currencies = ['Доллар', 'Сом', 'Рубль', 'Евро'];
+  final List<String> types = ['Банк', 'Касса'];
+  final List<String> names = ['Бакай банк', 'Мбанк', 'Офис касса'];
 
-  bool isFormValid = false;
   String? selectedName;
   String? selectedType;
   String? selectedCurrency;
 
+  bool isFormValid = false;
+
   void checkFormValidity() {
     setState(() {
       isFormValid =
-          nameController.text.isNotEmpty &&
-          typeController.text.isNotEmpty &&
+          selectedName != null &&
+          selectedType != null &&
           selectedCurrency != null &&
+          selectedName!.isNotEmpty &&
+          selectedType!.isNotEmpty &&
           selectedCurrency!.isNotEmpty;
-      selectedType != null && selectedType!.isNotEmpty;
-      selectedName != null && selectedName!.isNotEmpty;
     });
   }
-
-  @override
-  void initState() {
-    super.initState();
-    nameController.addListener(checkFormValidity);
-    typeController.addListener(checkFormValidity);
-  }
-
-  @override
-  void dispose() {
-    nameController.dispose();
-    typeController.dispose();
-    super.dispose();
-  }
-
-  final List<String> currencies = ['Доллар', 'Сом', 'Рубль', 'Евро'];
-
-  final List<String> types = ['банк', 'касса'];
-
-  final List<String> names = ['Бакай Банк', 'Мбанк', 'Офис касса'];
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +38,7 @@ class _AddAccountPageState extends State<AddAccountPage> {
       appBar: AppBar(
         backgroundColor: AppColors.backroundColor,
         centerTitle: true,
-        title: Text('Добавить счет', style: AppTextStyles.f24w600),
+        title: Text('Редактировать счет', style: AppTextStyles.f24w600),
         leadingWidth: 100,
         leading: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -73,74 +54,37 @@ class _AddAccountPageState extends State<AddAccountPage> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 50,
-            decoration: BoxDecoration(
-              color: AppColors.backroundColor,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            DropDownFormField(items: names),
+            const SizedBox(height: 12),
+
+            DropDownFormField(items: types),
+            const SizedBox(height: 12),
+
+            DropDownFormField(items: currencies),
+            const SizedBox(height: 24),
+
+            ElevatedButton(
+              onPressed:
+                  isFormValid
+                      ? () {
+                        // Сактоо логикасы
+                      }
+                      : null,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColors.primaryColor,
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
+              child: const Text('Сохранить'),
             ),
-          ),
-          30.h,
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              children: [
-                DropdownFormField(
-                  label: 'Название',
-                  currencies: names,
-                  value: selectedName,
-                  onChanged: (value) {
-                    setState(() {
-                      selectedName = value;
-                    });
-                    checkFormValidity();
-                  },
-                ),
-                12.h,
-                DropdownFormField(
-                  label: 'Тип',
-                  currencies: types,
-                  value: selectedType,
-                  onChanged: (value) {
-                    setState(() {
-                      selectedType = value;
-                    });
-                    checkFormValidity();
-                  },
-                ),
-                12.h,
-                // Вот поле с Dropdown
-                DropdownFormField(
-                  label: 'Валюта',
-                  currencies: currencies,
-                  value: selectedCurrency,
-                  onChanged: (value) {
-                    setState(() {
-                      selectedCurrency = value;
-                    });
-                    checkFormValidity();
-                  },
-                ),
-                24.h,
-                ElevatedButtonWidget(
-                  text: 'Сохранить',
-                  onPressed:
-                      isFormValid
-                          ? () {
-                            // Save action
-                          }
-                          : null,
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
