b700b0949b8232b45f5577a24cc4c76d24a8837d
mamarasulov <bvjcjcgjf@gmail.com>
2025-05-31 11:19:34 +0600

    router ad

--- lib/src/feature/menu/presentation/pages/settings/counterparties_page.dart
+++ lib/src/feature/menu/presentation/pages/settings/counterparties_page.dart
@@ -1,3 +1,7 @@
+import 'dart:developer';
+
+import 'package:aps_mobile/src/core/core.dart';
+import 'package:aps_mobile/src/feature/feature.dart';
 import 'package:flutter/material.dart';
 
 class CounterpartiesPage extends StatelessWidget {
@@ -5,6 +9,137 @@ class CounterpartiesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final List<Map<String, String>> accounts = [
+      {'name': 'ИП Жаркынай', 'type': 'Клиент'},
+      {'name': 'ИП Егор', 'type': 'Сотрудник'},
+      {'name': 'Макамбаев', 'type': 'Поставщик'},
+      {'name': 'ИП Егор', 'type': 'Поставщик'},
+      {'name': 'ИП Егор', 'type': 'Сотрудник'},
+    ];
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      appBar: CustomAppBar(
+        title: 'Контрагенты',
+        backgroundColor: AppColors.backroundColor,
+      ),
+      body: Column(
+        children: [
+          DecoratedBox(
+            decoration: BoxDecoration(
+              color: AppColors.backroundColor,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Column(
+                children: [
+                  20.h,
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.pushNamed(context, AppRoutes.addAccount);
+                          },
+                          label: const Text(
+                            'Добавить контрагента',
+                            style: AppTextStyles.f16w500,
+                          ),
+                          icon: const Icon(Icons.add, size: 20),
+                          iconAlignment: IconAlignment.end,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.primaryColorLight,
+                            foregroundColor: Colors.white,
+                            fixedSize: const Size(double.infinity, 48),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  12.h,
+                  Row(
+                    children: [
+                      OutlinedButtonWidget(
+                        onPressed: () {},
+                        text: 'Распечатать',
+                      ),
+                      12.w,
+                      OutlinedButtonWidget(
+                        onPressed: () {},
+                        text: 'Скачать в Excel',
+                      ),
+                    ],
+                  ),
+                  20.h,
+                ],
+              ),
+            ),
+          ),
+          12.h,
+
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 0.1),
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: DataTable(
+              showCheckboxColumn: true,
+              showBottomBorder: true,
+              headingRowColor: WidgetStateProperty.all(Colors.black),
+              headingTextStyle: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+              columns: const [
+                DataColumn(
+                  label: Text('Название', style: AppTextStyles.f16w500),
+                ),
+                DataColumn(label: Text('Тип', style: AppTextStyles.f16w500)),
+                DataColumn(label: Text('')), // для меню с тремя точками
+              ],
+              rows:
+                  accounts.map((account) {
+                    return DataRow(
+                      cells: [
+                        DataCell(
+                          Text(account['name']!, style: AppTextStyles.f16w500),
+                        ),
+                        DataCell(
+                          Text(account['type']!, style: AppTextStyles.f16w500),
+                        ),
+
+                        DataCell(
+                          PopupMenuWid(
+                            context: context,
+                            tapDelete: () {
+                              ShowSheet().showDeleteDialog(
+                                context,
+                                accountName: account['name']!,
+
+                                onConfirm: () {
+                                  log('Удаляем: ${account['name']}');
+                                },
+                                title: 'Удалить счет',
+                              );
+                            },
+                            tapEdit: () {
+                              Navigator.pushNamed(
+                                context,
+                                AppRoutes.editAccount,
+                              );
+                            },
+                          ),
+                        ),
+                      ],
+                    );
+                  }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
