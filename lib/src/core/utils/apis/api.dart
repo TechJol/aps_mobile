4e46771e5f8117f13e4de3d86a9650c7c2a408e8
mamarasulov <bvjcjcgjf@gmail.com>
2025-06-04 09:51:00 +0600

    api added

---
+++ lib/src/core/utils/apis/api.dart
@@ -0,0 +1,9 @@
+class AppApi {
+  static const String baseUrl = 'api.softkgpro.online/';
+
+  static String get url => baseUrl;
+
+  static String get login => '${baseUrl}api/login/';
+
+  static String get register => '${baseUrl}api/register/';
+}
