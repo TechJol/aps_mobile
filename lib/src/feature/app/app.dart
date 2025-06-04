8b9e917341b01911b386f87701da943c407590d2
mamarasulov <bvjcjcgjf@gmail.com>
2025-06-04 15:00:07 +0600

    bloc add and repo

--- lib/src/feature/app/app.dart
+++ lib/src/feature/app/app.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
         onGenerateRoute: RouteGenerator.onGenerate,
-        initialRoute: 'main',
+        initialRoute: '/',
       ),
     );
   }
